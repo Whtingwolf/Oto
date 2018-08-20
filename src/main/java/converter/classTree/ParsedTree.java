0d59e2aa2cdc2e238a7278c8f31ef11bee56fdc9
withing <13640151155@163.com>
2018-08-20 10:41:17 +0800

    modify: fix field type bug

--- src/main/java/converter/classTree/ParsedTree.java
+++ src/main/java/converter/classTree/ParsedTree.java
@@ -2,14 +2,31 @@ package converter.classTree;
 
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
 
 public class ParsedTree {
 
     private BinaryTree<Pair> root;
 
+    private static final HashSet<Class> PRIMITIVE_CLASS = new HashSet<>();
+
+    static {
+        PRIMITIVE_CLASS.add(Integer.TYPE);
+        PRIMITIVE_CLASS.add(Double.TYPE);
+        PRIMITIVE_CLASS.add(Float.TYPE);
+        PRIMITIVE_CLASS.add(Short.TYPE);
+        PRIMITIVE_CLASS.add(Boolean.TYPE);
+        PRIMITIVE_CLASS.add(Byte.TYPE);
+        PRIMITIVE_CLASS.add(Character.TYPE);
+        PRIMITIVE_CLASS.add(String.class);
+
+    }
+
+    enum PRIMITIVE{
+    }
+
     public ParsedTree(String className) {
         root = new BinaryTree<>();
-
     }
 
     public ParsedTree(Object object) {
@@ -18,7 +35,7 @@ public class ParsedTree {
         Field[] fields = clazz.getDeclaredFields();
         for (Field f : fields) {
             Pair pair = new Pair(f.getName(), f.getType());
-            if (isBasalType(f.getDeclaringClass())) {
+            if (isBasalType(f.getType())) {
                 try {
                     pair.setValue(f.get(object));
                 } catch (IllegalAccessException e) {
@@ -30,6 +47,9 @@ public class ParsedTree {
     }
 
     public boolean isBasalType(Class clazz) {
-        return true;
+        if(PRIMITIVE_CLASS.contains(clazz)){
+            return true;
+        }
+        return false;
     }
 }
